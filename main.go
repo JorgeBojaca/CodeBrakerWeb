5ae11e6397c1c431a3f381f9913a928cbeb29ed7
JorgeBojaca <jorgebojaca@gmail.com>
2017-04-22 10:35:50 -0500

    Creación de API

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+	router := gin.Default()
+
+	router.GET("/codebreaker/setup/:number", func(c *gin.Context){
+		number := c.Param("number")
+		setSecret(number)
+		c.String(http.StatusOK, "secret number configured: %s",number)
+	})
+
+	 router.GET("/codebreaker/guess/:number", func(c *gin.Context) {
+		name := c.Param("number")
+		result := validate(name)
+		c.String(http.StatusOK, "Answer: %s", result)
+	})
+
+	router.Run(":" + port)
+}
